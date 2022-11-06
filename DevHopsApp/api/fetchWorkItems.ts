0dec34c30b1976beb901d89d9efba030de65c50c
Chuck Driesler <cdriesler.iv@gmail.com>
2022-11-06 01:36:22 -0400

    fix fetch

--- DevHopsApp/api/fetchWorkItems.ts
+++ DevHopsApp/api/fetchWorkItems.ts
@@ -5,6 +5,7 @@ export const fetchWorkItems = async (): Promise<WorkItem[]> => {
    return data.map((d: any) => {
     d.status = d.status.toLowerCase()
     d.statusUpdates.forEach((s: any) => (s.status = s.status.toLowerCase()))
+    d.statusUpdates.sort((a: any, b: any) => (a.updateTime < b.updateTime) ? -1 : ((a.updateTime > b.updateTime) ? 1 : 0)).reverse()
     return d
    })
 }
