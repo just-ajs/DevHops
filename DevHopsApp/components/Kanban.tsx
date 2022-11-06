46c8e7a42fba0da1162b8c0f6270532dec67ffcc
Chuck Driesler <cdriesler.iv@gmail.com>
2022-11-05 23:17:04 -0400

    comments button

--- DevHopsApp/components/Kanban.tsx
+++ DevHopsApp/components/Kanban.tsx
@@ -114,9 +114,13 @@ export const Kanban = ({ workItems }: KanbanProps): React.ReactElement => {
                             <div className='w-full h-36 rounded-md bg-medium'>
                             {getFirstWorkItemImage(item) ? <img src={`data:image/png;base64,${getFirstWorkItemImage(item)}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }}/> : null}
                             </div>
-                            <div className='w-full pl-2 mt-3 mb-1 flex justify-start items-center'>
-                                <svg height={14} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#333333" d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z"/></svg>
-                                <p className="ml-2 font-sans text-xs text-slate">{item.statusUpdates.length}</p>
+                            <div className='w-full flex justify-start items-center'>
+                                <button className='mt-1 p-2 flex justify-start items-center rounded-md hover:bg-medium' onClick={(e) => {
+                                    // Show comments
+                                }}>
+                                    <svg height={14} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#333333" d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z"/></svg>
+                                    <p className="ml-2 font-sans text-xs text-slate">{item.statusUpdates.length}</p>
+                                </button>
                             </div>
                         </div>
                     </Draggable>
