70e797f3f4be23fffe122d4ed92746711a5dbe5e
Chuck Driesler <cdriesler.iv@gmail.com>
2022-11-06 10:05:34 -0500

    header

--- DevHopsApp/components/KanbanContainer.tsx
+++ DevHopsApp/components/KanbanContainer.tsx
@@ -14,8 +14,9 @@ export const KanbanContainer = ({ children }: KanbanContainerProps): React.React
     return (
 
             <div className='w-full h-full flex flex-col justify-start items-center'>
-                <div className='w-full h-12 flex justify-start items-center'>
-                    {/* <Image src="/dev-hops-logo.png" alt="DevHops logo" width={50} height={25} /> */}
+                <div className='w-full pl-5 pt-2 pb-2 h-14 flex justify-start items-center bg-light bg-opacity-30'>
+                    <Image src="/dev-hops-square.svg" alt="DevHops logo" width={50} height={25} />
+                    <h1 className='ml-2 font-sans text-2xl text-brand font-semibold'>DevHops Kanban Board</h1>
                 </div>
                 <div className='w-full flex-grow p-5 grid gap-5' style={{ gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'}}>
                     {children}
