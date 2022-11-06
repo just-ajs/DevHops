067efee0d978d3fe5dca01e328de647a5360bd76
Chuck Driesler <cdriesler.iv@gmail.com>
2022-11-06 08:52:29 -0500

    CORS

--- DevHopsBackEnd/DevHopsBackEnd/DevHopsBackEnd/Program.cs
+++ DevHopsBackEnd/DevHopsBackEnd/DevHopsBackEnd/Program.cs
@@ -14,7 +14,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+    {
+        builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    }));
+
 var app = builder.Build();
+
+app.UseCors("corsapp");
 
 
 
