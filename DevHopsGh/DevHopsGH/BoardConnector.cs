241372180cef4caf09cdbe6ba8242e290bbe8a90
just-ajs <35227625+just-ajs@users.noreply.github.com>
2022-11-06 02:02:00 +0000

    another component add

---
+++ DevHopsGh/DevHopsGH/BoardConnector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace DevHopsGH
+{
+    public class BoardConnector
+    {
+
+        public string GetWorkItemById(string id)
+        {
+            System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(AppConstants.getWorkItemUrl);
+
+            request.Method = "GET";
+            request.Accept = "text/plain";
+            System.Net.WebResponse response = request.GetResponse();
+            string responseString;
+            using (var streamReader = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                responseString = streamReader.ReadToEnd();
+            }
+
+            return responseString;
+
+        }
+
+        public string UpdateStatus(string json)
+        {
+            // send status update
+            HttpWebRequest request = (HttpWebRequest)System.Net.WebRequest.Create(AppConstants.sendUpdateUrl);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.Accept = "text/plain";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+
+            var response = request.GetResponse();
+            var result = "";
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+
+    }
+}
--- DevHopsGh/DevHopsGH/Components/UpdateStatusComponent.cs
+++ DevHopsGh/DevHopsGH/Components/UpdateStatusComponent.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using Grasshopper.Kernel;
+using Newtonsoft.Json;
 using Rhino.Geometry;
 
 namespace DevHopsGH.Components
@@ -22,6 +25,11 @@ namespace DevHopsGH.Components
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddTextParameter("Task ID", "ID", "Kanban board id task", GH_ParamAccess.item);
+            pManager.AddTextParameter("Status", "Status", "Status to be updated", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Screenshot", "Screenshot", "Screenshot", GH_ParamAccess.item);
+            pManager.AddTextParameter("Add comment", "comment", "comment", GH_ParamAccess.item);
+
         }
 
         /// <summary>
@@ -29,6 +37,8 @@ namespace DevHopsGH.Components
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddTextParameter("Status updated", "Status updated", "Status status", GH_ParamAccess.item);
+
         }
 
         /// <summary>
@@ -37,6 +47,63 @@ namespace DevHopsGH.Components
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            string id = String.Empty;
+            string status  = String.Empty;
+            bool sendScreenshot  = false;
+            string comment = String.Empty;
+
+            if (!DA.GetData(0, ref id)) return;
+            if (!DA.GetData(1, ref status)) return;
+            if (!DA.GetData(2, ref sendScreenshot)) return;
+            if (!DA.GetData(3, ref comment)) return;
+
+            // post method, id and status
+            //send status
+            string filepath = @"C:\Users\jszychowska\Dropbox\devhops\images\image.jpeg";
+
+            string imageString = String.Empty;
+            
+            //read folder images
+            if (sendScreenshot)
+            {
+                using (Image image = Image.FromFile(filepath))
+                {
+                    using (MemoryStream m = new MemoryStream())
+                    {
+                        image.Save(m, image.RawFormat);
+                        byte[] imageBytes = m.ToArray();
+
+                        // Convert byte[] to Base64 String
+                        string base64String = Convert.ToBase64String(imageBytes);
+                        imageString = base64String;
+
+                    }
+                }
+            }
+
+
+            var username = Environment.UserName;
+            // post image, status
+
+            WorkItemUpdate workItemUpdate = new WorkItemUpdate();
+            workItemUpdate.workitemId = id;
+            workItemUpdate.timestamp = DateTime.Now.ToString();
+            workItemUpdate.username = Environment.UserName;
+
+            if (comment != String.Empty)
+            {
+                workItemUpdate.comment = comment;
+            }
+
+            Status myStatus;
+            Enum.TryParse(status, out myStatus);
+            workItemUpdate.status = myStatus;
+
+            if (sendScreenshot) workItemUpdate.image = imageString;
+
+            string sendUpdateJson = System.Text.Json.JsonSerializer.Serialize(workItemUpdate);
+            BoardConnector boardConnector = new BoardConnector();
+            boardConnector.UpdateStatus(sendUpdateJson);
         }
 
         /// <summary>
